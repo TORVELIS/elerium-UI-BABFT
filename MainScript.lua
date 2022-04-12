50fcb0a4c68cae232c8a7344bc767e82e9b1ce7a
TORVELIS <89320445+TORVELIS@users.noreply.github.com>
2022-04-12 16:30:57 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,11 +1,21 @@
 -- https://github.com/H3xad3cimalDev/imgui_library
-local Player = game:GetService('Players').LocalPlayer
+local plr = game.Players.LocalPlayer
+local Character = plr.Character
+local HMR = Character.HumanoidRootPart
+local hum = Character.Humanoid
+
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/TORVELIS/elerium-UI-BABFT/main/EZ.lua'))();
 local NotifLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/TORVELIS/elerium-UI-BABFT/main/NotificUI.lua'))();
 local Window = Library:AddWindow('BABFT (Yön timo)')
+
 
 local tab = Window:AddTab('Home')
 local tab2 = Window:AddTab('Farm')
 local tab3 = Window:AddTab('Player')
 local tab4 = Window:AddTab('Teleports')
 local tab5 = Window:AddTab('Misc')
+tab:AddButton("Copy Discord Link", function()
+    local N1 = NotifLib.new("success", "Copied to clipboard", "Copied DISCORDLINK to clipboard.")
+    N1:deleteTimeout(3)
+    
+end)
