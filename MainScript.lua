4f91bed779ed94c94552ca97f246a38a157caf2d
TORVELIS <89320445+TORVELIS@users.noreply.github.com>
2022-04-12 17:57:41 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,9 +1,4 @@
 -- https://github.com/H3xad3cimalDev/imgui_library
-local plr = game.Players.LocalPlayer
-local Character = plr.Character
-local HMR = Character.HumanoidRootPart
-local hum = Character.Humanoid
-
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/TORVELIS/elerium-UI-BABFT/main/EZ.lua'))();
 local NotifLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/TORVELIS/elerium-UI-BABFT/main/NotificUI.lua'))();
 local Window = Library:AddWindow('BABFT (Yön timo)')
@@ -14,20 +9,44 @@ local tab2 = Window:AddTab('Farm')
 local tab3 = Window:AddTab('Player')
 local tab4 = Window:AddTab('Teleports')
 local tab5 = Window:AddTab('Misc')
+
+function setWalkSpeed(WS)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = WS
+end
+
+function setJumpPower(JP)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = JP
+end
+
 tab:AddButton("Copy Discord Link", function()
     setclipboard("discord.gg/QzgbdcXYaP")
     local N1 = NotifLib.new("success", "Copied to clipboard", "Copied discord.gg/QzgbdcXYaP to clipboard.")
     N1:deleteTimeout(3)
-
 end)
 
+local slider = tab3:AddSlider("WalkSpeed", function(p)
+	setWalkSpeed(p)
+    end,{
+	["min"] = 16,
+	["max"] = 500,
+})	slider:Set(0)
+
+local slider = tab3:AddSlider("JumpPower", function(p)
+	setJumpPower(p)
+    end,{
+	["min"] = 50,
+	["max"] = 500,
+})	slider:Set(0)
 
 
-
-
+local slider = tab5:AddSlider("Max Zoom", function(p)
+	game.Players.LocalPlayer.CameraMaxZoomDistance = p
+    end,{
+	["min"] = 400,
+	["max"] = 10000,
+})	slider:Set(400)
 
 tab5:AddButton("Destroy GUI", function()
-
-game:GetService("CoreGui").imgui:Destroy
-
+    game:GetService("CoreGui").imgui:Destroy()
 end)
+
