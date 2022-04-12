956d705fb927f9aaea13c621f8b32af185c8585d
TORVELIS <89320445+TORVELIS@users.noreply.github.com>
2022-04-12 16:09:14 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,24 +1,11 @@
 -- https://github.com/H3xad3cimalDev/imgui_library
 local Player = game:GetService('Players').LocalPlayer
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/TORVELIS/elerium-UI-BABFT/main/EZ.lua'))();
+local NotifLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/TORVELIS/elerium-UI-BABFT/main/NotificUI.lua'))();
 local Window = Library:AddWindow('BABFT (Yön timo)')
-local Tab = Window:AddTab('Home')
-local Tab2 = Window:AddTab('Farm')
-local Tab3 = Window:AddTab('Player')
-local Tab4 = Window:AddTab('Teleports')
-local Tab5 = Window:AddTab('Misc')
-local Farm = false
-local pickedArea
-local arealiste = {'1';'2';'3';'4'}
 
-local Switch = Tab2:AddSwitch('Autofarm', function(bool)
-    Farm = bool
-end)
-
-local Dropdown = Tab2:AddDropdown('Area', function(Area)
-    pickedArea = Area
-end)
-
-for i,v in pairs (arealiste) do
-    Dropdown:Add(v)
-end
+local tab = Window:AddTab('Home')
+local tab2 = Window:AddTab('Farm')
+local tab3 = Window:AddTab('Player')
+local tab4 = Window:AddTab('Teleports')
+local tab5 = Window:AddTab('Misc')
