c84ac2ea1bc427ed78f63af601d34bdfd438d884
NotNaeNaed <65853230+NotNaeNaed@users.noreply.github.com>
2022-04-13 12:24:59 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -15,8 +15,47 @@ local tab5 = Window:AddTab('Misc')
 
 _G.TweenSpeed = 3
 _G.AutoFarm = false
+_G.deleted = false
 
 -- funcs
+
+function Run_Farm()
+    local client = game.Players.LocalPlayer
+    local platform = Instance.new("Part", client.Character)
+    platform.Anchored = true
+    platform.Transparency = 1
+    platform.Size = Vector3.new(6, 1, 6)
+
+    local connection
+    connection = game:GetService("RunService").RenderStepped:connect(function()
+        if client.Character:FindFirstChild("HumanoidRootPart") then
+            platform.Position = client.Character.HumanoidRootPart.CFrame * Vector3.new(0, -3.5, 0)
+        else
+            connection:Disconnect()
+        end
+    end)
+
+    task.spawn(function()
+        task.wait(2)
+        firetouchinterest(client.Character.HumanoidRootPart, workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, 0)
+    end)
+
+    for i=1,10 do
+        if 1+1 == 3 then 
+            return 
+        else
+            client.Character.HumanoidRootPart.CFrame = workspace.BoatStages.NormalStages["CaveStage" .. i].DarknessPart.CFrame + Vector3.new(0, -30, 0)
+            
+            task.wait(1)
+
+            if workspace.BoatStages.NormalStages:FindFirstChild("CaveStage" .. i + 1) then
+                client.Character.HumanoidRootPart.CFrame = workspace.BoatStages.NormalStages["CaveStage" .. i + 1].DarknessPart.CFrame + Vector3.new(0, -30, -20)
+            end
+        
+            task.wait(0.5)
+        end
+    end
+end
 
 function setWalkSpeed(WS)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = WS
@@ -26,30 +65,12 @@ function setJumpPower(JP)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = JP
 end
 
-function TweenToCFrame(TweenSpeed, CFrameToGo)
-    if not game.Players.LocalPlayer.Character.Humanoid then return end
-    local tween =  game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(TweenSpeed), {CFrame = CFrameToGo})
-    tween:Play()
-    tween.Completed:Wait()
-end
-
-function AutoFarmFunc()
-    --TweenToCFrame(_G.TweenSpeed, CFrame.new()) --
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 1369.09009, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --  
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 2139.09009, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --  
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 2909.09009, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --  
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 3679.09009, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --  
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 4449.08984, 1, 0, 0, 0, 1, 0, 0, 0, 1)) -- 
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 5219.08984, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --  
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 5989.08984, 1, 0, 0, 0, 1, 0, 0, 0, 1)) -- 
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 6759.08984, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 7529.08984, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-51.5656433, 100.0000458, 8299.08984, 1, 0, 0, 0, 1, 0, 0, 0, 1)) --
-    TweenToCFrame(_G.TweenSpeed, CFrame.new(-55.7065125, -358.739624, 9492.35645, 0, 0, -1, 0, 1, 0, 1, 0, 0)) --
-    wait(5)
-end
 
 -- tab1
+
+tab:AddLabel("Script credits: Torvelis, Naenaed\n (made everything other than fast autofarm and uiLib)")
+tab:AddLabel("Credits: Zinzox, Singularity (from v3rmillion)")
+tab:AddLabel("Notification Library: Jxereas")
 
 tab:AddButton("Copy Discord Link", function()
     setclipboard("discord.gg/QzgbdcXYaP")
@@ -59,21 +80,17 @@ end)
 
 --tab 2
 
-local switch = tab2:AddSwitch("AutoFarm", function(bool)
+tab2:AddSwitch("Fast Autofarm", function(bool)
+    _G.AutoFarm = bool
     spawn(function()
-        while _G.Autofarm == true do
-            AutoFarmFunc()
-            wait(5)
-        end
+        repeat
+            Run_Farm()
+            wait(3)
+        until _G.AutoFarm == false
     end)
 end)
 
-local slider = tab2:AddSlider("Autofarm speed (lower=more fast)", function(p)
-	_G.TweenSpeed = p
-    end,{
-	["min"] = 3,
-	["max"] = 20,
-})	slider:Set(0)
+tab2:AddLabel("WARNING: Let Autofarm run to the end after disabling!")
 
 --tab3
 
@@ -91,18 +108,31 @@ local slider = tab3:AddSlider("JumpPower", function(p)
 	["max"] = 500,
 })	slider:Set(0)
 
+-- local slider3 = tab3:AddSlider("Max Zoom", function(p)
+-- 	game.Players.LocalPlayer.CameraMaxZoomDistance = p
+--     end,{
+-- 	["min"] = 400,
+-- 	["max"] = 10000,
+-- }) slider3:Set(0)
+
 --tab5
 
 tab5:AddButton("Destroy GUI", function()
     game:GetService("CoreGui").imgui:Destroy()
 end)
 
-local slider = tab5:AddSlider("Max Zoom", function(p)
-	game.Players.LocalPlayer.CameraMaxZoomDistance = p
-    end,{
-	["min"] = 400,
-	["max"] = 10000,
-})	slider:Set(0)
+tab5:AddButton("Remove water damage (reset to disable)", function()
+    game.Players.LocalPlayer.Character.WaterDetector:Destroy()
+end)
 
-
-
+tab5:AddButton("Remove stages (rejoin to disable)", function()
+    if _G.deleted ~= true then
+        game:GetService("Workspace").BoatStages.OtherStages:Destroy()
+        local notifN2 = NotifLib.new("Success", "Succesfully removed stages", 'Removed stages (must rejoin to undo)')
+        notifN2:deleteTimeout(3.5)
+        _G.deleted = true
+    else
+        local notifN3 = NotifLib.new("error", "Failed to remove stages", "Failed to remove stages")
+        notifN3:deleteTimeout(3.5)
+    end
+end)
